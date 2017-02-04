056f83a5bfc93b51410c6a61df999ac9ff5b4276
jan åge johnsen <jaj@skill.no>
2017-02-04 15:29:58 +0100

    More refactoring of commands

--- matrix-console/src/components/App.js
+++ matrix-console/src/components/App.js
@@ -1,79 +1,11 @@
 import React, { Component } from 'react';
 import Terminal from 'react-bash';
-import Pad from 'pad';
+
+
+import {show, dodge, open, WhoAmI} from './Commands';
 
 export let data = {};
-export let GlobalData = {};
-
-export const dodge = {
-    exec: ({ structure, history, cwd }, command) => {
-        parent.window.Xrm.Utility.openWebResource("skill_loading");
-    }
-};
-
-export const WhoAmI = {
-    exec: ({ structure, history, cwd }, command) => {
-        let ret = ["The choosen one!"];
-        return { structure, cwd, history: history.concat( ...ret.map(value => ({ value })) )};
-    },
-};
-
-export const open = {
-    exec: ({ structure, history, cwd }, command) => {
-        let ret = [];
-
-        if(command.args && command.args[0]) {
-            parent.window.Xrm.Utility.openEntityForm("contact", command.args[0], null, {openInNewWindow: true});
-        }
-        else {
-            ret.push("Invalid command use: open [GUID]");
-        }
-
-        return { structure, cwd, history: history.concat( ...ret.map(value => ({ value })) )};
-    },
-};
-
-export const show = {
-    exec: ({ structure, history, cwd }, command) => {
-        let ret = [];
-        let matches = [];
-
-        if(GlobalData["contacts"] == undefined) {
-            ret.push("Please hold on... Connecting to the matrix...");
-        }
-        else if(command.args && command.args[0] == "dreamers") {
-            ret.push("Finding dreamers...");
-
-            matches = GlobalData["contacts"].filter((contact) => {
-                return contact.preferredcontactmethodcode == 1
-            });
-        }
-        else if(command.args && command.args[0] == "agents") {
-            ret.push("Finding agents...");
-
-            matches = GlobalData["contacts"].filter((contact) => {
-                return contact.preferredcontactmethodcode == 2
-            });
-        }
-        else {
-            ret.push("Invalid command use: show [dreamers|agents]");
-        }
-
-        if(matches.length > 0) {
-            if(matches.length > 10) {
-                ret.push(`OMG, found ${matches.length} matches... Showing first 10: `);
-                matches = matches.slice(0, 10);
-            }
-
-            matches.forEach(contact => {
-                var name = Pad(contact.fullname, 40,".");
-                ret.push(`${name} GUID: ${contact.contactid}`);
-            })
-        }
-
-        return { structure, cwd, history: history.concat( ...ret.map(value => ({ value })) )};
-    },
-};
+window.GlobalData = {};
 
 const extensions = { dodge, show, open, WhoAmI };
 
@@ -83,10 +15,8 @@ export default class App extends Component {
     }
     
     componentDidMount() {
-        console.log("componentDidMount", Pad("test", 100));
         this.props.service.getData().then(contacts => {
-            console.log("GOT DATA!!!")
-            GlobalData["contacts"] = contacts;
+            window.GlobalData["contacts"] = contacts;
         });
     }
 
--- matrix-console/src/components/Commands.js
+++ matrix-console/src/components/Commands.js
@@ -1,3 +1,5 @@
+import Pad from 'pad';
+
 export const dodge = {
     exec: ({ structure, history, cwd }, command) => {
         parent.window.Xrm.Utility.openWebResource("skill_loading");
@@ -20,6 +22,48 @@ export const open = {
         }
         else {
             ret.push("Invalid command use: open [GUID]");
+        }
+
+        return { structure, cwd, history: history.concat( ...ret.map(value => ({ value })) )};
+    },
+};
+
+export const show = {
+    exec: ({ structure, history, cwd }, command) => {
+        let ret = [];
+        let matches = [];
+
+        if(window.GlobalData["contacts"] == undefined) {
+            ret.push("Please hold on... Connecting to the matrix...");
+        }
+        else if(command.args && command.args[0] == "dreamers") {
+            ret.push("Finding dreamers...");
+
+            matches = window.GlobalData["contacts"].filter((contact) => {
+                return contact.preferredcontactmethodcode == 1
+            });
+        }
+        else if(command.args && command.args[0] == "agents") {
+            ret.push("Finding agents...");
+
+            matches = GlobalData["contacts"].filter((contact) => {
+                return contact.preferredcontactmethodcode == 2
+            });
+        }
+        else {
+            ret.push("Invalid command use: show [dreamers|agents]");
+        }
+
+        if(matches.length > 0) {
+            if(matches.length > 10) {
+                ret.push(`OMG, found ${matches.length} matches... Showing first 10: `);
+                matches = matches.slice(0, 10);
+            }
+
+            matches.forEach(contact => {
+                var name = Pad(contact.fullname, 40,".");
+                ret.push(`${name} GUID: ${contact.contactid}`);
+            })
         }
 
         return { structure, cwd, history: history.concat( ...ret.map(value => ({ value })) )};
